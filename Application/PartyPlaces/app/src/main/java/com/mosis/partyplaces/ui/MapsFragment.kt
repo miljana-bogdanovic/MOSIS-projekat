c7582420348aba4bd8042e3c2020ccbca86a3f74
miljana-bogdanovic <bogmiljana@gmail.com>
2022-06-12 13:57:48 +0200

    created footer menu

--- Application/PartyPlaces/app/src/main/java/com/mosis/partyplaces/ui/MapsFragment.kt
+++ Application/PartyPlaces/app/src/main/java/com/mosis/partyplaces/ui/MapsFragment.kt
@@ -27,9 +27,11 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
     }
 
     override fun onCreateView(
